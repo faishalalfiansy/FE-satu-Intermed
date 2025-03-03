f00af4fb1342e82be3978c109a816cacfe5d81a7
faishalalfiansy <faishalalfiansy@gmail.com>
2025-03-04 00:23:34 +0700

    new6

--- src/components/Molecules/Logindaftar/Judul.jsx
+++ src/components/Molecules/Logindaftar/Judul.jsx
@@ -1,5 +1,5 @@
-import HeaderCard from "src/components/Atom/HeaderCard";
-import SubJudul from "src/components/Atom/SubJudul";
+import HeaderCard from "../../Atom/HeaderCard";
+import SubJudul from "../../Atom/SubJudul";
 const Judul = (props) => {
     const { titleHeader, titleSubjudul, kelasjudul } = props
     return (
