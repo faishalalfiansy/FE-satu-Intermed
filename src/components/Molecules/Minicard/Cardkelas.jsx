e7bab588d3a6d75b8c2cd8f6fe04258827071fdb
faishalalfiansy <faishalalfiansy@gmail.com>
2025-03-06 06:13:21 +0700

    newdelapan

--- src/components/Molecules/Minicard/Cardkelas.jsx
+++ src/components/Molecules/Minicard/Cardkelas.jsx
@@ -14,15 +14,15 @@ const Cardkelas = (props) => {
             childrenone={childrenone}
             childrentwo="Mulai transformasi dengan instruktur Profesional, harga yang terjangkau, dan terpercaya"
           />
-          <Miniatastwo sumber="/src/assets/instruktur1.png" />
+          <Miniatastwo sumber="../../../../src/assets/instruktur1.png" />
         </section>
         <section className="card-akhir">
         <Minibawah
-          bintang1="/src/assets/full-star.png"
-          bintang2="/src/assets/full-star.png"
-          bintang3="/src/assets/full-star.png"
-          bintang4="/src/assets/setengah-star.png"
-          bintang5="/src/assets/kosong-star.png"
+          bintang1="../../../../src/assets/full-star.png"
+          bintang2="../../../../src/assets/full-star.png"
+          bintang3="../../../../src/assets/full-star.png"
+          bintang4="../../../../src/assets/setengah-star.png"
+          bintang5="../../../../src/assets/kosong-star.png"
           children1="4.5(86)"
           children2="Rp 300K"
         />
