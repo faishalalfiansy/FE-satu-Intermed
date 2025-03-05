dcdd02113d9e698c6ff04b435933d3cbdc3c8bdf
faishalalfiansy <faishalalfiansy@gmail.com>
2025-03-06 05:59:04 +0700

    newenam

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import ass from 'vite-plugin-asset-injector'
 
 // https://vite.dev/config/
 export default defineConfig({
