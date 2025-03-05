b83322d53f7d21550c340e4084e2b8dd80c5ed43
faishalalfiansy <faishalalfiansy@gmail.com>
2025-03-06 05:24:32 +0700

    newsatu

--- src/components/Organisme/Isiberanda.jsx
+++ src/components/Organisme/Isiberanda.jsx
@@ -32,39 +32,39 @@ const Isiberanda = () => {
       </section>
       <div className="cardbatas">
       <Cardkelas
-          sumber="/public/card1.jpg"
+          sumber="/src/assets/card1.jpg"
           childrenone="IT Fullstack Developer"
         />
         <Cardkelas
-          sumber="/public/card2.jpg"
+          sumber="/src/assets/card2.jpg"
           childrenone="UI/UX Designer"
         />
         <Cardkelas
-          sumber="/public/card3.jpg"
+          sumber="/src/assets/card3.jpg"
           childrenone="Pemasaran Digital"
         />
         <Cardkelas
-          sumber="/public/card4.jpg"
+          sumber="/src/assets/card4.jpg"
           childrenone="Pengembangan Diri"
         />
         <Cardkelas
-          sumber="/public/card5.jpg"
+          sumber="/src/assets/card5.jpg"
           childrenone="Bisnis Digital"
         />
         <Cardkelas
-          sumber="/public/card6.jpg"
+          sumber="/src/assets/card6.jpg"
           childrenone="Pemasaran Digital"
         />
         <Cardkelas
-          sumber="/public/card7.jpg"
+          sumber="/src/assets/card7.jpg"
           childrenone="Pengembangan Diri"
         />
         <Cardkelas
-          sumber="/public/card8.jpg"
+          sumber="/src/assets/card8.jpg"
           childrenone="Bisnis Digital"
         />
         <Cardkelas
-          sumber="/public/card9.jpg"
+          sumber="/src/assets/card9.jpg"
           childrenone="Pemasaran Digital"
         />
       </div>
